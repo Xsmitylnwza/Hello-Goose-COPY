ca93788d08962d201397f82bbc52541d3b1aa47c
realFK98 <wishira2013.coth@gmail.com>
2023-10-09 01:07:07 +0700

    fix-text-about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
      <img className="absolute top-1 w-screen -z-20 "
       src="/images/about/Cloud.PNG" alt="cloud" />
       <div className=" flex-col  w-screen">
-       <p className="font-cherry text-[64px]  text-center mb-5 text-white">Welcom to Hello <br/> World Goose!</p>
+       <p className="font-cherry text-[64px]  text-center mb-5 text-white">Welcom to <br/>Hello World Goose!</p>
        <div className="about-data flex flex-row">
         <img src="/images/about/Goose.PNG"className="w-1/5 ml-16"  alt="" />
         <div className="w-3/4 flex flex-row items-center justify-center">
