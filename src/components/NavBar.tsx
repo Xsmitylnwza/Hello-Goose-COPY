75aa559b65e727a3821f6ebd364f1d3bc177ffb5
Sitthata <aprils96950@gmail.com>
2023-10-08 16:22:50 +0700

    Initial

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,19 @@
-import NavMenu from "./NavMenu"
-
+import NavMenu from "./NavMenu";
 
 const NavBar = () => {
   return (
-    <div className="w-full px-8 flex-between">
-        <img src="../public/images/Logo.PNG" alt="Logo" className="w-[20%] h-full" />
+    <div className="fixed top-0 w-full">
+      <div className="flex px-[7.5rem] py-4 flex-between">
+        {/* <img
+          src="../public/images/Logo.PNG"
+          alt="Logo"
+          className="w-[20%] h-full"
+        /> */}
+        <h1>Logo</h1>
         <NavMenu />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
