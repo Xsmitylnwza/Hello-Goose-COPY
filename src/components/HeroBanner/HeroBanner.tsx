9f62c2f9057d702a5ad2f7d9bec8a1b0a760e026
Sitthata <aprils96950@gmail.com>
2023-10-09 00:06:13 +0700

    Adjust variable

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -8,16 +8,14 @@ const HeroBanner = () => {
     target: containerRef,
     offset: ["start start", "end start"]
   });
-  const gooseY = useTransform(scrollYProgress, [0, 1], [0, -200]);
-  const gooseX = useTransform(scrollYProgress, [0, 1], [0, -200]);
-  const textY = useTransform(scrollYProgress, [0, 1], [0, -200]);
+  const transfromY = useTransform(scrollYProgress, [0, 1], [0, -200]);
   return (
     <div ref={containerRef} className="relative min-h-screen bg-bottom bg-no-repeat bg-cover flex-center-col bg-background-Hero">
-      <HeroLogo translateY={textY}/>
+      <HeroLogo translateY={transfromY}/>
       <img className="absolute top-0 w-screen"
       src="/images/hero/Cloud.PNG" alt="cloud" />
       <motion.img className="hidden absolute right-[15%] w-1/6 md:block md:bottom-[2%] lg:bottom-[5%]" 
-      src="/images/hero/goose-element_0004_Layer-6.png" alt="goose hero" style={{y: gooseY, x: gooseX}}/>
+      src="/images/hero/goose-element_0004_Layer-6.png" alt="goose hero" style={{y: transfromY, x: transfromY}}/>
       <img className="absolute bottom-0 w-screen"
       src="/images/hero/Rock.PNG" alt="rock"/>
     </div>
