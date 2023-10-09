f275992b76ac37011ea177ecb0767424a53ee77a
Sitthata <aprils96950@gmail.com>
2023-10-09 21:30:57 +0700

    Update hover state

--- src/components/HeroBanner/HeroLogo.tsx
+++ src/components/HeroBanner/HeroLogo.tsx
@@ -4,12 +4,23 @@ type HeroLogoProps = {
   translateY: MotionValue<number>;
 };
 
-const HeroLogo = ({translateY}: HeroLogoProps) => {
-  const fadeIn = {
+const HeroLogo = ({ translateY }: HeroLogoProps) => {
+  const logoVariants = {
     hidden: { opacity: 0, y: 100, scale: 0 },
     visible: { opacity: 1, y: 0, scale: 1, transition: { duration: 1, ease: 'backInOut' } },
-  }
-  return <motion.img src="/images/logo.webp" alt="Logo" className="h-full md:w-[45%] w-[65%] z-10" variants={fadeIn} initial="hidden" animate="visible" style={{y: translateY}}/>;
+    handleOnHover: {
+      scale: 1.15, transition: { duration: 0.25 }
+    }
+  };
+  return <motion.img 
+    src="/images/hero/Logo.PNG" alt="Logo" 
+    className="h-full md:w-[45%] w-[65%] z-10 cursor-pointer lg:w-[40%]" 
+    variants={logoVariants} 
+    initial="hidden" 
+    animate="visible" 
+    style={{ y: translateY }} 
+    whileHover="handleOnHover" 
+  />;
 };
 
 export default HeroLogo;
