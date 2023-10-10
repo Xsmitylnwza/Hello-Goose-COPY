44499e28e1aaebe2334689773900395c8561a577
realFK98 <wishira2013.coth@gmail.com>
2023-10-10 16:03:27 +0700

    I think it done yet [about]

--- src/components/About/AboutText.tsx
+++ src/components/About/AboutText.tsx
@@ -1,7 +1,7 @@
 const AboutText = function () {
   return (
     <div className="flex w-3/4 flex-row items-center justify-center md:justify-start   ">
-      <div className="w-6/6 flex h-max  flex-col items-center  justify-center rounded-[30px] bg-white p-10 pb-20  opacity-75  md:h-5/6 md:w-5/6">
+      <div className="w-6/6 flex h-max  flex-col items-center  justify-center rounded-[30px] bg-white p-10 pb-[100px]  opacity-75  md:h-5/6 md:w-5/6">
         <p className="mb-20 mt-4 pb-20 text-center  font-itim text-[18px] md:mb-0 md:mt-10 md:pb-0">
           ยินดีต้อนรับน้องห่านทุกคน เข้าสู่โครงการ SIT HelloWorld-Goose
           ที่จะพาน้อง ๆ มาเรียนรู้วิธีการสร้าง การออกแบบ และพัฒนา
--- src/components/About/GooseImg.tsx
+++ src/components/About/GooseImg.tsx
@@ -8,7 +8,7 @@ const winks = [
 const GooseImg = function () {
   return (
     <div className=" absolute -bottom-2 z-10  flex w-2/5 items-center justify-center md:static  md:bottom-0 md:justify-start">
-      <div className=" relative -bottom-12 min-w-[250px]   md:-bottom-0 md:left-[18%] ">
+      <div className=" relative -bottom-12 min-w-[250px]   md:-bottom-0 lg:left-[18%] ">
         <img src="/images/about/Goose.PNG" className="mx-auto  w-1/2" alt="" />
         {winks.map((wink, index) => (
           <WinkEffect
