455494cfc4b39c44fb394aec13fb3ce73e183c2a
Sitthata <aprils96950@gmail.com>
2023-10-12 22:35:11 +0700

    Smooth transition animation on nav

--- src/components/About/TitleAbout.tsx
+++ src/components/About/TitleAbout.tsx
@@ -1,6 +1,9 @@
-const TitleAbout = function () {
+
+const TitleAbout = () => {
   return (
-    <p className="  mobileL:text-[48px] mobileM:mb-5 top-[-10%] -mb-4 text-center   font-cherry text-[24px] text-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] md:mb-0 md:text-[64px]">
+    <p 
+      className="top-[-10%] -mb-4 text-center font-cherry text-[24px]   text-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] mobileM:mb-5 mobileL:text-[48px] md:mb-0 md:text-[64px]"
+    >
       Welcome to <br />
       Hello World Goose!
     </p>
--- src/components/Nav/NavItemUnderline.tsx
+++ src/components/Nav/NavItemUnderline.tsx
@@ -1,18 +1,23 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 type NavItemUnderlineProps = {
   active?: boolean;
 };
 
 const NavItemUnderline = ({ active }: NavItemUnderlineProps) => {
-  if (!active) return null;
 
   return (
-    <motion.img
-      src="/images/nav/underline.png"
-      alt="Underline"
-      className="absolute -bottom-2 left-0 w-full translate-y-1/2 transform transition-all duration-500 ease-in-out"
-    />
+    <AnimatePresence>
+      {active && <motion.img
+        src="/images/nav/underline.png"
+        alt="Underline"
+        className="absolute left-0 w-full transform translate-y-1/2 -bottom-10"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      />}
+    </AnimatePresence>
   );
 };
 
