cc84db5711e9245f4a8a9dacc924a2c8feb8d971
realFK98 <wishira2013.coth@gmail.com>
2023-10-12 18:06:32 +0700

    fixBug

--- src/components/About/AboutText.tsx
+++ src/components/About/AboutText.tsx
@@ -1,7 +1,7 @@
 const AboutText = function () {
   return (
     <div className="flex w-3/4 flex-row items-center justify-center md:justify-start   ">
-      <div className="w-6/6 mobileL:pb-[100px] mobileL:pt-10  flex h-max  flex-col items-center justify-center rounded-[30px] bg-white px-5 pb-0  opacity-75  md:h-5/6 md:w-5/6">
+      <div className="w-6/6 mobileL:pb-[140px] mobileL:pt-10  flex h-max  flex-col items-center justify-center rounded-[30px] bg-white px-5 pb-0  opacity-75  md:h-5/6 md:w-5/6">
         <p className="mobileL:mb-20 mobileL:text-[18px] mb-0 mt-4 pb-[30px] text-center font-itim  text-[12px] leading-[29px] md:mb-0 md:mt-10 md:pb-0">
           ยินดีต้อนรับน้องห่านทุกคน เข้าสู่โครงการ SIT HelloWorld-Goose
           ที่จะพาน้อง ๆ มาเรียนรู้วิธีการสร้าง การออกแบบ และพัฒนา
--- src/components/About/GooseImg.tsx
+++ src/components/About/GooseImg.tsx
@@ -8,7 +8,7 @@ const winks = [
 const GooseImg = function () {
   return (
     <div className="mobileL:-bottom-2 absolute bottom-[-77px] z-10  flex w-2/5 items-center justify-center md:static  md:bottom-0 md:justify-start">
-      <div className="mobileL:min-w-[250px] relative -bottom-12  min-w-[163px]   md:-bottom-0 lg:left-[18%] ">
+      <div className="mobileL:min-w-[305px] relative -bottom-12  min-w-[163px]   md:-bottom-0 lg:left-[18%] ">
         <img src="/images/about/Goose.PNG" className="mx-auto  w-1/2" alt="" />
         {winks.map((wink, index) => (
           <WinkEffect
