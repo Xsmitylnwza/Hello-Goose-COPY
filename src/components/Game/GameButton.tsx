1c8311c3b1184d0029486dc5ed997d4ce87f3030
Sitthata <aprils96950@gmail.com>
2023-10-15 20:10:46 +0700

    Added animation on game page

--- src/components/Game/GameButton.tsx
+++ src/components/Game/GameButton.tsx
@@ -1,10 +1,47 @@
+import { motion } from "framer-motion";
+
 const GameButton = () => {
+  const gameButton = {
+    initial: {
+      opacity: 0,
+      scale: 0,
+    },
+    animate: {
+      opacity: 1,
+      scale: 1,
+    },
+    hover: {
+      scale: 1.1,
+    },
+    tap: {
+      rotate: 2,
+      scale: 0.95,
+      transition: {
+        type: "spring",
+        stiffness: 400,
+        damping: 17,
+        duration: 0.1,
+      },
+    },
+  };
+
   return (
-      <img 
-        className="h-full md:w-[50%] w-[70%] z-10 cursor-pointer lg:w-[45%] hover:drop-shadow-[0_0_50px_#fff]" 
-        src="/images/game/เกม.png" 
-        alt="Game Link" 
+    <a
+      href=""
+      className="z-10 h-full w-[70%] cursor-pointer md:w-[50%] lg:w-[45%]"
+    >
+      <motion.img
+        className="hover:drop-shadow-[0_0_50px_#fff]"
+        src="/images/game/เกม.png"
+        alt="Game Link"
+        variants={gameButton}
+        initial="initial"
+        animate="animate"
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ duration: 0.2 }}
       />
+    </a>
   );
 };
 
